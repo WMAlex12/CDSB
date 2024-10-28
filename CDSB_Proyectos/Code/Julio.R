9f1948f62c101ca85beb7c5677ed49fdde29080f
JulioAntonio <julio@MacBook-Pro-de-Julio.local>
2024-10-28 13:47:59 -0700

    "Agrega el script Julio.R"

---
+++ CDSB_Proyectos/Code/Julio.R
@@ -0,0 +1,8 @@
+# Crear un mensaje
+mensaje <- "¡Hola, este es mi primer script en R en GitHub!"
+
+# Guardar el mensaje en un archivo de texto
+write(mensaje, file = "mensaje.txt")
+
+# Imprimir mensaje en consola
+print(mensaje)
